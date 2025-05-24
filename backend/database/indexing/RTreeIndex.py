25af00f3d31eb1a4dfd91eaa7456f9ced2a761d8
Jvnc0503 <jvnc0503@gmail.com>
2025-05-24 00:24:11 -0500

    Created constructor

--- backend/database/indexing/RTreeIndex.py
+++ backend/database/indexing/RTreeIndex.py
@@ -1,13 +1,30 @@
 import os
 from rtree import index
-from typing import Tuple, List
+import json
+from .IndexRecord import IndexRecord
+from . import utils
 
 class RTreeIndex:
-    def __init__(self, table_name: str, field_name: str):
-        self.index_name = f"{table_name}.{field_name}.rtree"
-        if not os.path.exists(self.index_name + ".idx"):
-            raise FileNotFoundError(f"El índice {self.index_name} no existe. Crea el índice primero.")
+    def __init__(self, table_name: str, indexed_field: str):
+        # Validate scheme and field
+        self.schema = utils.load_schema(table_name)
+        if indexed_field not in [f["name"] for f in self.schema["fields"]]:
+            raise ValueError(f"Campo {indexed_field} no existe en el schema de {table_name}")
+    
+        self.filename = f"{table_name}.{indexed_field}.rtree.idx"
+
+        # Validate if index file exists
+        if not os.path.exists(self.filename):
+            raise FileNotFoundError(f"El índice RTree para {table_name} en el campo {indexed_field} no existe. Crea el índice primero.")
         
+        self.key_format = utils.get_key_format_from_schema(self.schema, indexed_field)
+
+        # Create index using library
         props = index.Property()
         props.storage = index.RT_Disk
-        self.idx = index.Index(self.index_name, properties=props)
+        self.idx = index.Index(self.filename, properties = props)
+
+
+    @staticmethod
+    def build_index(heap_filename: str, extract_index_fn, key_field: str):
+        pass
