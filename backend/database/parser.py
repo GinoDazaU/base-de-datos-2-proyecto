fee41fde40be597115a285c85f60676bc7642d09
DanielCasquino <daniel.casquino@utec.edu.pe>
2025-05-25 20:27:38 -0500

    wip: select from parsing

--- backend/database/parser.py
+++ backend/database/parser.py
@@ -1,12 +1,12 @@
 from mo_sql_parsing import parse
 import json
-from database import create_table, check_table_exists, get_table_schema, insert_record
-from storage.HeapFile import HeapFile
+from database import create_table, check_table_exists, get_table_schema, insert_record, get_table_path
 import pandas as pd
 from enum import Enum
 from future.schema import SchemaType
 from future.primitives import Varchar
 from storage.Record import Record
+from storage.HeapFile import HeapFile
 
 class DEBUG_ENABLE(Enum):
     NO = 0
@@ -20,7 +20,7 @@ def print_debug(message):
         print(message)
 
     
-TableAbstraction = str | pd.DataFrame | None
+TableAbstraction = tuple[str, pd.DataFrame | None] # maps alias to name,df
 Level = dict[str, TableAbstraction]
 
 class Environment:
@@ -30,8 +30,11 @@ class Environment:
             self.current_level = other.current_level
         else:
             self.levels = {}
-            self.levels[0] = Level()
+            self.levels[0] = Level() # maps int to {name, table} dict
             self.current_level = 0
+
+    def __repr__(self):
+        return f"Environment(levels={self.levels}, current_level={self.current_level})"
     
     def descend(self):
         self.current_level += 1
@@ -43,8 +46,6 @@ class Environment:
         self.current_level -= 1
         self.levels.pop(self.current_level + 1)
 
-environment = Environment()
-
 class QueryResult:
     def __init__(self, success: bool, data: TableAbstraction, message:str = None):
         self.success = success
@@ -54,8 +55,10 @@ class QueryResult:
     def __repr__(self):
         return f"QueryReturnType(success={self.success}, data={self.data}, message={self.message})"
 
+environment = Environment()
+
 class Query:
-    def __init__(self, tree, level: int = 0):
+    def __init__(self, tree: dict, level: int):
         self.tree = tree
         self.level = level
 
@@ -64,6 +67,8 @@ class Query:
             first_clause_type = "create table"
         elif "insert" in self.tree:
             first_clause_type = "insert"
+        elif "select" in self.tree:
+            first_clause_type = "select"
             
         print_debug(f"Root clause type is {first_clause_type}")
 
@@ -72,6 +77,8 @@ class Query:
                 return self.evaluate_create_table(self.tree["create table"])
             case "insert":
                 return self.evaluate_insert(self.tree)
+            case "select":
+                return self.evaluate_select(self.tree)
             case _:
                 raise Exception(f"Unknown root clause type: {first_clause_type}")
 
@@ -162,7 +169,67 @@ class Query:
             return QueryResult(success=True, data=None, message=f"Record inserted into table {name} successfully")
         except Exception as e:
             raise Exception(f"Failed to insert record into table {name}: {e}")
+    
+    def evaluate_select(self, clause) -> QueryResult:
+        # first we need to handle from clause
+        try:
+            self.evaluate_from(clause["from"]) # sets up environment
+        except Exception as e:
+            raise Exception(f"Failed to evaluate from clause: {e}")
+        
+        selectionizer: dict[str, list[str]] = {} # perry the platypus??
+        for column_select_clause in clause["select"]:
+            attribute = column_select_clause["value"]
+            identifier, column = attribute.split(".")
+            if identifier not in environment.levels[self.level]:
+                raise Exception(f"Identifier {identifier} not found in environment at level {self.level}")
+            # we already know the tables exist, but we haven't checked columns yet
+            schema: SchemaType = get_table_schema(environment.levels[self.level][identifier][0])
+            # now we check if column exists (god i wish schema was a dict and not a list of tuples)
+            # print_debug(column)
+            # print_debug(schema)
+            if column not in [col[0] for col in schema]:
+                raise Exception(f"Column {column} does not exist in table/aliased {identifier}")
+            selectionizer[identifier] = selectionizer.get(identifier, []) + [column]
+        
+        # which columns to get from which tables in environment level
 
+        if "where" in clause:
+            # uoggggggggghhhhhhhhhhhhhhhh
+            pass
+        aaa = next(iter(selectionizer.items()))
+        return self.handle_select(aaa[0], aaa[1]) # rfdcgfsraseasdsa
+
+    def handle_select(self, name: str, columns: list[str]) -> QueryResult:
+        print_debug(f"Selecting {columns} from {name}")
+        if environment.levels[self.level][name][1] is None: # if table has not been deserialized yet
+            og_name = environment.levels[self.level][name][0]
+            heap = HeapFile(get_table_path(og_name))
+            environment.levels[self.level][name] = (og_name, HeapFile.to_dataframe(heap)) # tuples are immutable
+        df: pd.DataFrame = environment.levels[self.level][name][1]
+        df = df[columns] if columns else df
+        return QueryResult(success=True, data=df, message=f"Selected {columns} from {name} successfully")
+
+    def evaluate_from(self, from_clause):
+        # this does not handle nested selects yet
+        if not isinstance(from_clause, list):
+            from_clause = [from_clause] # wrap in list to handle single table case
+
+        for item in from_clause:
+            if isinstance(item, str):
+                if not check_table_exists(item):
+                    raise Exception(f"Table {item} does not exist")
+                environment.levels[self.level][item] = (item, None) # store at level [x] -> name: name
+            elif isinstance(item, dict):
+                # handles alias
+                og_name = item["value"]
+                if not check_table_exists(og_name):
+                    raise Exception(f"Table {og_name} does not exist")
+                alias = item["name"]
+                environment.levels[self.level][alias] = (og_name, None) # store at level [x] -> alias: name (to later fetch actual df but keep alias)
+            else:
+                raise Exception(f"Unknown from clause type: {type(item)}")
+    
 
 class Parser:
     def __init__(self):
@@ -181,10 +248,19 @@ class Parser:
     
 def main():
     parser = Parser()
-    query_create_table = "create table student (id int, name varchar(20), age int);"
-    insert_query = "insert into student (id, name, age) values (1, 'John Doe', 20);"
-    result = parser.parse(insert_query)
+
+    create_table = "create table student (id int, name varchar(20), age int);"
+    create_table_2 = "create table teacher (id int, name varchar(20), age int, subject varchar);"
+
+    insert = "insert into student (id, name, age) values (1, 'John Doe', 20);"
+    insert_2 = "insert into teacher (id, name, age, subject) values (1, 'John Doe', 30, 'Mathematics');"
+
+    select_simple = "select student.id, name from student;"
+    select_complex = "select student.id, t.id, t.name from student, teacher as t where student.name = t.name;"
+
+    result = parser.parse(select_complex)
     print(result.message)
+    print(result.data)
 
 if __name__ == "__main__":
     main()
